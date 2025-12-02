27f73ffcb9217a3f63fbe54303c1b92905fe4066
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-12-02 20:36:06 +0800

    Update diy.sh

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -16,14 +16,14 @@ sed -i "s/openwrt-mediatek-filogic/kwrt-mediatek-filogic/g" target/linux/mediate
 sed -i "s/ fitblk / /g" target/linux/mediatek/image/filogic.mk
 
 # 禁用小容量设备
-echo "🔧 禁用小容量设备..."
-if [ -f ".config" ]; then
-  sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_re3000-v1=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_re3000-v1 is not set/' .config
-  sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_wr3000-v1=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_wr3000-v1 is not set/' .config
-  sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_netgear_wax220=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_netgear_wax220 is not set/' .config
-  echo "✅ 配置修改完成"
-else
-  echo "⚠️ .config 文件不存在，跳过设备禁用"
-fi
+# echo "🔧 禁用小容量设备..."
+# if [ -f ".config" ]; then
+#   sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_re3000-v1=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_re3000-v1 is not set/' .config
+#   sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_wr3000-v1=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_wr3000-v1 is not set/' .config
+#   sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_netgear_wax220=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_netgear_wax220 is not set/' .config
+#   echo "✅ 配置修改完成"
+# else
+#   echo "⚠️ .config 文件不存在，跳过设备禁用"
+# fi
 
-echo "✅ 所有修改完成"
+# echo "✅ 所有修改完成"
