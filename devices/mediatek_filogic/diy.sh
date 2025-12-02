acc209de63439a3ed2b73911b5d77e3b56e53f95
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-12-02 12:47:21 +0800

    Update diy.sh

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -15,3 +15,7 @@ sed -i "s/-stock//g" package/boot/uboot-envtools/files/mediatek_filogic
 sed -i "s/openwrt-mediatek-filogic/kwrt-mediatek-filogic/g" target/linux/mediatek/image/filogic.mk
 sed -i "s/ fitblk / /g" target/linux/mediatek/image/filogic.mk
 
+# 禁用那几个报错的小容量设备
+sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_re3000-v1=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_re3000-v1 is not set/' .config
+sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_wr3000-v1=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_cudy_wr3000-v1 is not set/' .config
+sed -i 's/CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_netgear_wax220=y/# CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_netgear_wax220 is not set/' .config
