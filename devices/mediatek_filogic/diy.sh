e6a908926d1298532e8c6c5a3b8180d1bd2e99aa
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-12-05 11:01:41 +0800

    Update diy.sh

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -3,11 +3,6 @@
 shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-#bash $SHELL_FOLDER/../common/kernel_6.6.sh
-
-# 全局替换所有 6.6.117 → 6.6.116
-find . -type f \( -name "*.mk" -o -name "Makefile" -o -name "kernel-*" \) -exec sed -i 's/\([ =:]\)6\.6\.117/\16.6.116/g' {} + 2>/dev/null
-
 sed -i -E -e 's/ ?root=\/dev\/fit0 rootwait//' -e "/rootdisk =/d" -e '/bootargs.* = ""/d' target/linux/mediatek/dts/*{qihoo-360t7,netcore-n60,h3c-magic-nx30-pro,jdcloud-re-cp-03,cmcc-rax3000m,jcg-q30-pro,tplink-tl-xdr*}.dts
 
 find target/linux/mediatek/filogic/base-files/ -type f -exec sed -i "s/-stock//g" {} \;
